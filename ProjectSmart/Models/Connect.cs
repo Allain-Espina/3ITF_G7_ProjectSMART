1a86b99dc278399aed41b551506678def8d62bad
JanDrefner <jandrefner.santos.cics@ust.edu.ph>
2023-12-01 00:10:44 +0800

    Added Working Connect Page

--- ProjectSmart/Models/Connect.cs
+++ ProjectSmart/Models/Connect.cs
@@ -1,15 +1,21 @@
-﻿namespace ProjectSmart.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectSmart.Models
 {
     public enum Tag
     {
-        Registration_Form, Transcript_of_Records, Terminal_Report, Gratitude_Letter, Others
+        Registration_Form, Certified_Grades, Terminal_Report, Gratitude_Letter, Others
     }
     public class Connect
     {
-        public string ConnectId { get; set; }
-        public string StudentID { get; set; }
-        public string ConnectSubject { get; set; }
+        [Key]
+        public int ConnectId { get; set; }
+        [Display(Name = "Scholar's Email Address")]
+        [Required]
+        public string ScholarEmailAddress { get; set; }
         public Tag ConnectTag { get; set; }
+        [Required]
+        [Display(Name = "Comment")]
         public string ConnectContent { get; set; }
     }
 }
